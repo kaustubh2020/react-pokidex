3f467aa492a0de72626b54b0a20aa16f4995d764
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2024-10-13 13:35:45 +0530

    Prop validation

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,32 @@
+import PropTypes from "prop-types";
+
 const PokemonCard = ({ id, image, gif, name, type }) => {
-  const style = type + " thumb-container";
+  const style = `${type} thumb-container`;
+
   return (
     <div className={style}>
       <div className="number">
         <small>#{id}</small>
         <div>
-          <img src={gif} alt={name} width={20} height={20} />
+          <img src={gif} alt={name} width={20} height={20} loading="lazy" />
         </div>
       </div>
-      <img src={image} alt={name} width={120} height={120} />
+      <img src={image} alt={name} width={120} height={120} loading="lazy" />
       <div className="detail-wrapper">
         <h3>{name}</h3>
         <small>Type: {type}</small>
       </div>
     </div>
   );
+};
+
+// Prop validation
+PokemonCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  gif: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 
 export default PokemonCard;
